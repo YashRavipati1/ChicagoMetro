67d1b47a77d85a572d02154d31261a1b56ed2c45
Arjun Ashok <arjun3.ashok@gmail.com>
2023-07-14 20:41:53 -0700

    Added rel position functionality

--- map_visualizer.py
+++ map_visualizer.py
@@ -1,36 +1,44 @@
 """
     This file defines the functionality for displaying any set of metro 
-    stations given their longitude and latitude coordinates.
+    stations given their longitude and latitude coordinates. To be run as a 
+    script.
 """
 
 
 # import libraries #
 import networkx as nx
 import matplotlib as plt
+import json
 
 
 # calculate coordinates #
 # setup
-TL_CORNER_COORD = (-1,  -1)
-BR_CORNER_COORD = (-1,  -1)
+BR_CORNER_COORD = (1000,  1000)     # using traditional pixel position as matrix
 COORDS_PATH = './.json'
 STATIONS_PATH = './secondary.json'
 
 # execute setup
-wstart = TL_CORNER_COORD[0]
-hstart = BR_CORNER_COORD[1]
-width = BR_CORNER_COORD[0] - wstart
-height = TL_CORNER_COORD[1] - hstart
+width = BR_CORNER_COORD[0]
+height = BR_CORNER_COORD[1]
 
-with open(COORDS_PATH) as f:
+with open(COORDS_PATH, 'r') as f:
     coords_dict = json.load(f)
-with open(STATIONS_PATH) as f:
+with open(STATIONS_PATH, 'r') as f:
     stations_dict = json.load(f)
 
 # calculate positions
 coords = dict()
-
 for station_id, station_name in stations_dict.items():
-    # calculate new coordinate
-    coord = 
+    # check already calculated
+    if station_name in coords:
+        continue
 
+    # calculate new coordinate
+    coord = coords_dict[station_name]
+    rel_pos = (coords[0] / width, coords[1] / height)
+    coords[station_name] = rel_pos
+
+
+# save results #
+with open('pixel_positions.json', 'w') as f:
+    json.dump(coords, f, indent=4)
