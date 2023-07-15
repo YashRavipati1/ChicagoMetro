0e305b8f305373caa7bfda75372127c673a5680b
Arjun Ashok <arjun3.ashok@gmail.com>
2023-07-15 15:00:47 -0700

    just shoot me bruh idk what i've been doing

--- create_graph.py
+++ create_graph.py
@@ -8,34 +8,28 @@ with open("clean_stations_distance.json") as f:
 graph = nx.Graph()
 
 color_maps = {
-    "A": "Rose",
-    "I": "Blue",
-    "S": "Leaf",
-    "E": "Magenta",
-    "G": "Orange",
-    "M": "Red",
-    "H": "Silver",
-    "T": "Sky",
-    "C": "Green",
-    "Y": "Gold",
-    "Z": "Purple",
-    "N": "Emerald",
-    "F": "Brown",
+    "BASE": "0.5",
+    "A":    "Blue",
+    "I":    "Lime",
+    "S":    "Magenta",
+    "E":    "Orange",
+    "G":    "Red",
+    "M":    "Silver",
+    "H":    "Skyblue",
+    "T":    "Green",
+    "C":    "Gold",
+    "Y":    "Purple",
+    "Z":    "Darkgreen",
+    "N":    "Brown",
+    "F":    "Yellow"
 }
 
 for node, neighbors in data.items():
-    graph.add_node(node, color=color_maps[node[0]])
+    graph.add_node(node, color=color_maps["BASE"])
     for neighbor, weight in neighbors.items():
-        graph.add_edge(node, neighbor, weight=weight, color=color_maps[node[0]])
+        graph.add_edge(node, neighbor, weight=weight, color=color_maps["BASE"])
     # count += 1
     # if count == 5:
     #     break
 
 nx.write_graphml(graph, "tokyometro.graphml")
-
-
-pos = nx.spring_layout(graph)
-nx.draw(graph, pos, with_labels=True)
-edge_labels = nx.get_edge_attributes(graph, "weight")
-nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_labels)
-plt.show()
--- map_visualizer.py
+++ map_visualizer.py
@@ -9,36 +9,62 @@
 import networkx as nx
 import matplotlib as plt
 import json
+import * from dijkstras.py
 
 
 # calculate coordinates #
 # setup
-BR_CORNER_COORD = (1000,  1000)     # using traditional pixel position as matrix
-COORDS_PATH = './.json'
+BR_CORNER_COORD = (2526,  1785)     # using traditional pixel position as matrix
+COORDS_PATH = './pixels.json'
 STATIONS_PATH = './secondary.json'
+SCALE = 1
 
 # execute setup
 width = BR_CORNER_COORD[0]
 height = BR_CORNER_COORD[1]
 
 with open(COORDS_PATH, 'r') as f:
-    coords_dict = json.load(f)
+    coords = json.load(f)
 with open(STATIONS_PATH, 'r') as f:
     stations_dict = json.load(f)
 
-# calculate positions
-coords = dict()
-for station_id, station_name in stations_dict.items():
-    # check already calculated
-    if station_name in coords:
-        continue
+# # calculate positions
+# coords = dict()
+# for station_id, station_name in stations_dict.items():
+#     # check already calculated
+#     if station_name in coords:
+#         continue
 
-    # calculate new coordinate
-    coord = coords_dict[station_name]
-    rel_pos = (coords[0] / width, coords[1] / height)
-    coords[station_name] = rel_pos
+#     # calculate new coordinate
+#     coord = coords_dict[station_name]
+#     rel_pos = (coords[0] / width, coords[1] / height)
+#     coords[station_name] = rel_pos
 
 
 # save results #
 with open('pixel_positions.json', 'w') as f:
     json.dump(coords, f, indent=4)
+
+
+# show metro map #
+# scale relative positions
+coords = {k: v * SCALE for k, v in coords.items()}
+
+# get path
+path = dijkstras()
+
+edge_colors = nx.get_edge_attributes(graph, 'color').values()
+node_colors = nx.get_node_attributes(graph, 'color').values()
+widths = nx.get_edge_attributes(graph, 'weight')
+
+nx.draw_networkx(
+    graph,
+    pos=pos,
+    node_size=50,
+    # width=widths,
+    node_color=node_colors,
+    edge_color=edge_colors,
+    with_labels=True
+)
+# nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_labels)
+plt.show()
